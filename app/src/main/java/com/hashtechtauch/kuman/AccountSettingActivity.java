33a929925a4b486fa91b66a2106f6b6549fa7124
Meidie <46555331+Alloftheurlisalreadyused@users.noreply.github.com>
2020-06-07 16:50:53 +0700

    XML
    activity main added

---
+++ app/src/main/java/com/hashtechtauch/kuman/AccountSettingActivity.java
@@ -0,0 +1,106 @@
+package com.hashtechtauch.kuman;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class AccountSettingActivity extends AppCompatActivity
+{
+    private String strImage, strNama, strEmail, strId;
+    private String jsonLati, jsonLong;
+    private FirebaseFirestore mFirebaseFirestore;
+    private final static int REQUEST_PLACE_PICKER = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_setting);
+        ImageView accountImage = findViewById(R.id.profilePicture);
+        TextView accountName = findViewById(R.id.tvName);
+        TextView accountEmail = findViewById(R.id.tvEmail);
+
+        TextView counter = findViewById(R.id.counter);
+        Button btn_Back = findViewById(R.id.backBtn);
+        Button btn_Change_Name = findViewById(R.id.changeNameBtn);
+        Button btn_Change_Location = findViewById(R.id.changeLocBtn);
+        Button btn_Toggle_Notification = findViewById(R.id.toggleNotifBtn);
+        Button btn_Remove_SharedPrefence = findViewById(R.id.removeUserPrefsBtn);
+
+        SharedPreferences sp = getSharedPreferences("Token",MODE_PRIVATE);
+        strNama = sp.getString("nama",null);
+        strEmail = sp.getString("email",null);
+        strImage = sp.getString("gambar",null);
+        strId = sp.getString("id",null);
+
+        Glide.with(this).load(strImage).into(accountImage);
+        accountName.setText(strNama);
+        accountEmail.setText(strEmail);
+
+        btn_Back.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        btn_Change_Name.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        btn_Change_Location.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        btn_Toggle_Notification.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        btn_Remove_SharedPrefence.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                SharedPreferences sp = getSharedPreferences("Token", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.clear();
+                editor.apply();
+
+                Toast.makeText(AccountSettingActivity.this, "Data successfully removed.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/hashtechtauch/kuman/LoginActivity.java
@@ -0,0 +1,142 @@
+package com.hashtechtauch.kuman;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity
+{
+    private FirebaseAuth mFirebaseAuth;
+    private CallbackManager mCallbackManager;
+    private static final String TAG = "Authenticate Token";
+    private AccessTokenTracker accessTokenTracker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();
+        Button login_Facebook = findViewById(R.id.fbSignInBtn);
+
+        login_Facebook.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this,
+                        Arrays.asList("email", "public_profile"));
+                LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                        handleFacebookAccessToken(loginResult.getAccessToken());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d(TAG, "facebook:onCancel");
+                        Toast.makeText(LoginActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Log.d(TAG, "facebook:onError", error);
+                        Toast.makeText(LoginActivity.this, "Error. Please try again later", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+            }
+        });
+
+        // Jika user sudah login, tidak perlu login lagi
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null)
+        {
+            SharedPreferences sp = getSharedPreferences("Token",MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("nama",user.getDisplayName());
+            editor.putString("email",user.getEmail());
+            editor.putString("gambar",user.getPhotoUrl().toString());
+            editor.putString("id",user.getUid());
+            editor.apply();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token)
+    {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            // Sign in success
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+
+                            // simpan token di sharedpreferences
+                            SharedPreferences sp = getSharedPreferences("Token",MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sp.edit();
+                            editor.putString("nama",user.getDisplayName());
+                            editor.putString("email",user.getEmail());
+                            editor.putString("gambar",user.getPhotoUrl().toString());
+                            editor.putString("id",user.getUid());
+                            editor.apply();
+
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+
+
+                        }
+                        else
+                        {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+}
