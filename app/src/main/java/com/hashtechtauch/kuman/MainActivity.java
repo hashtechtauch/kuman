83bfa6f83ff0d90246bf685c92792734fc22eee6
Stefandy <stefandy@binus.ac.id>
2020-06-08 07:34:19 +0700

    adding JSON parser
    add JSON Parser
    make locationTv clickable

--- app/src/main/java/com/hashtechtauch/kuman/MainActivity.java
+++ app/src/main/java/com/hashtechtauch/kuman/MainActivity.java
@@ -4,18 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.bumptech.glide.Glide;
+import com.google.android.libraries.places.api.model.Place;
+import com.hashtechtauch.kuman.model.Country;
+import com.rtchagas.pingplacepicker.PingPlacePicker;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity
 {
     private String strHomeImage;
+    private TextView CountryName, newConfirmed, totalConfirmed, newDeaths, totalDeaths, newRecovered, totalRecovered;
+    private String jsonLati, jsonLong, countryCode, link;
+    private final static int REQUEST_PLACE_PICKER = 1001;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,9 +54,30 @@ public class MainActivity extends AppCompatActivity
         AppCompatImageView btn_Home_Notif = findViewById(R.id.notifBtn);
         AppCompatButton btn_Scan = findViewById(R.id.scanBtn);
         AppCompatImageView homeProfilePicture = findViewById(R.id.image);
+        CountryName = findViewById(R.id.locationTv);
+        newConfirmed = findViewById(R.id.newCaseNumber);
+        totalConfirmed = findViewById(R.id.ConfirmedNumber);
+        totalDeaths = findViewById(R.id.deathsNumber);
+        totalRecovered = findViewById(R.id.recoveredNumber);
+        sharedPreferences = getSharedPreferences("Covid", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
 
         strHomeImage = getIntent().getStringExtra("gambar");
         Glide.with(this).load(strHomeImage).into(homeProfilePicture);
+
+        CountryName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    String[] permissionMessages = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+                    ActivityCompat.requestPermissions(MainActivity.this, permissionMessages, 1);
+                    showPlacePicker();
+                } else {
+                    showPlacePicker();
+                }
+            }
+        });
 
         btn_Home_Image.setOnClickListener(new View.OnClickListener()
         {
@@ -61,4 +109,124 @@ public class MainActivity extends AppCompatActivity
 
 
     }
+
+    private void showPlacePicker() {
+        PingPlacePicker.IntentBuilder builder = new PingPlacePicker.IntentBuilder();
+        builder.setAndroidApiKey("AIzaSyCVqW4F6qEEo8YZDmHEN2K8j6D3NtTX59g")
+                .setMapsApiKey("AIzaSyA6zli3v7J1vh9sdJf30WhePFrM7RLHYuc");
+
+        // If you want to set a initial location rather then the current device location.
+        // NOTE: enable_nearby_search MUST be true.
+        // builder.setLatLng(new LatLng(37.4219999, -122.0862462))
+
+        try {
+            Intent placeIntent = builder.build(MainActivity.this);
+            startActivityForResult(placeIntent, REQUEST_PLACE_PICKER);
+        } catch (Exception ex) {
+            Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == REQUEST_PLACE_PICKER) && (resultCode == RESULT_OK)) {
+            Place place = PingPlacePicker.getPlace(data);
+            if (place != null) {
+//                Toast.makeText(this, "You selected the place: " + place.getName(), Toast.LENGTH_SHORT).show();
+//                latitude.setText(String.valueOf(Objects.requireNonNull(place.getLatLng()).latitude));
+//                longitude.setText(String.valueOf(Objects.requireNonNull(place.getLatLng()).longitude));
+
+                jsonLati = String.valueOf(Objects.requireNonNull(place.getLatLng()).latitude);
+                jsonLong = String.valueOf(Objects.requireNonNull(place.getLatLng()).longitude);
+
+                Request(jsonLati, jsonLong);
+                CountryName.setText("Current Location : " + sharedPreferences.getString("country", ""));
+                newConfirmed.setText(sharedPreferences.getString("newConfirmed", "0"));
+                totalConfirmed.setText(sharedPreferences.getString("totalConfirmed","0"));
+                totalDeaths.setText(sharedPreferences.getString("totalDeaths","0"));
+                totalRecovered.setText(sharedPreferences.getString("totalRecovered","0"));
+            }
+        }
+    }
+
+    private void Request(String Latitude, String Longitude) {
+        link = "https://api.opencagedata.com/geocode/v1/json?q="+Latitude+"%2C"+Longitude+"&key=d091601131184a4db55b15acc8ee6b46";
+        String url = link;
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray array = response.getJSONArray("results");
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject obj = array.getJSONObject(i);
+                                JSONObject obj2 = obj.getJSONObject("components");
+
+                                countryCode = obj2.getString("country_code");
+                            }
+                            Check(countryCode);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        RequestSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+    private void Check(String countryCode) {
+        String url = "https://api.covid19api.com/summary";
+        final Country CountryData = new Country();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray array = response.getJSONArray("Countries");
+
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject obj2 = array.getJSONObject(i);
+                                if (obj2.getString("CountryCode").equalsIgnoreCase(countryCode)) {
+                                    CountryData.setCountry(obj2.getString("Country"));
+                                    CountryData.setCountryCode(obj2.getString("CountryCode"));
+                                    CountryData.setSlug(obj2.getString("Slug"));
+                                    CountryData.setNewConfirmed(obj2.getInt("NewConfirmed"));
+                                    CountryData.setTotalConfirmed(obj2.getInt("TotalConfirmed"));
+                                    CountryData.setNewDeaths(obj2.getInt("NewDeaths"));
+                                    CountryData.setTotalDeaths(obj2.getInt("TotalDeaths"));
+                                    CountryData.setNewRecovered(obj2.getInt("NewRecovered"));
+                                    CountryData.setTotalRecovered(obj2.getInt("TotalRecovered"));
+                                    CountryData.setDate(obj2.getString("Date"));
+                                    break;
+                                }
+                            }
+                            editor.putString("country", CountryData.getCountry());
+                            editor.putString("newConfirmed", String.valueOf(CountryData.getNewConfirmed()));
+                            editor.putString("totalConfirmed", String.valueOf(CountryData.getTotalConfirmed()));
+                            editor.putString("totalDeaths", String.valueOf(CountryData.getTotalDeaths()));
+                            editor.putString("totalRecovered", String.valueOf(CountryData.getTotalRecovered()));
+                            editor.commit();
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        RequestSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
 }
